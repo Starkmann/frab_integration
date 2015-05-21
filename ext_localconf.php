a4bfff306209d6d135e0408d79480304e6adc8f0
eike.starkmann <devnull@localhost>
2015-05-21 09:08:18 +0200

    Starting 'Version 4.5' branch

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,8 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Eike.' . $_EXTKEY,
+Tx_Extbase_Utility_Extension::configurePlugin(
+	$_EXTKEY,
 	'List',
 	array(
 		'Conference' => 'list, show, showEvent',
--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+Tx_Extbase_Utility_Extension::registerPlugin(
 	$_EXTKEY,
 	'List',
 	'Frab Conference'
@@ -11,12 +11,12 @@ if (!defined('TYPO3_MODE')) {
 
 $pluginSignature = str_replace('_','',$_EXTKEY) . '_list';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
+t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Frab Integration');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Frab Integration');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_frabintegration_domain_model_conference', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_conference.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_frabintegration_domain_model_conference');
+t3lib_extMgm::addLLrefForTCAdescr('tx_frabintegration_domain_model_conference', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_conference.xlf');
+t3lib_extMgm::allowTableOnStandardPages('tx_frabintegration_domain_model_conference');
 $GLOBALS['TCA']['tx_frabintegration_domain_model_conference'] = array(
 	'ctrl' => array(
 		'title'	=> 'LLL:EXT:frab_integration/Resources/Private/Language/locallang_db.xlf:tx_frabintegration_domain_model_conference',
@@ -39,13 +39,13 @@ $GLOBALS['TCA']['tx_frabintegration_domain_model_conference'] = array(
 			'endtime' => 'endtime',
 		),
 		'searchFields' => 'title,start,end,days_count,timeslot_duration,days,',
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Conference.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_conference.gif'
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Conference.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_conference.gif'
 	),
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_frabintegration_domain_model_room', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_room.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_frabintegration_domain_model_room');
+t3lib_extMgm::addLLrefForTCAdescr('tx_frabintegration_domain_model_room', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_room.xlf');
+t3lib_extMgm::allowTableOnStandardPages('tx_frabintegration_domain_model_room');
 $GLOBALS['TCA']['tx_frabintegration_domain_model_room'] = array(
 	'ctrl' => array(
 		'title'	=> 'LLL:EXT:frab_integration/Resources/Private/Language/locallang_db.xlf:tx_frabintegration_domain_model_room',
@@ -68,13 +68,13 @@ $GLOBALS['TCA']['tx_frabintegration_domain_model_room'] = array(
 			'endtime' => 'endtime',
 		),
 		'searchFields' => 'name,events,',
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Room.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_room.gif'
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Room.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_room.gif'
 	),
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_frabintegration_domain_model_person', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_person.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_frabintegration_domain_model_person');
+t3lib_extMgm::addLLrefForTCAdescr('tx_frabintegration_domain_model_person', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_person.xlf');
+t3lib_extMgm::allowTableOnStandardPages('tx_frabintegration_domain_model_person');
 $GLOBALS['TCA']['tx_frabintegration_domain_model_person'] = array(
 	'ctrl' => array(
 		'title'	=> 'LLL:EXT:frab_integration/Resources/Private/Language/locallang_db.xlf:tx_frabintegration_domain_model_person',
@@ -97,13 +97,13 @@ $GLOBALS['TCA']['tx_frabintegration_domain_model_person'] = array(
 			'endtime' => 'endtime',
 		),
 		'searchFields' => 'image,full_public_name,abstract,description,links,events,',
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Person.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_person.gif'
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Person.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_person.gif'
 	),
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_frabintegration_domain_model_day', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_day.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_frabintegration_domain_model_day');
+t3lib_extMgm::addLLrefForTCAdescr('tx_frabintegration_domain_model_day', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_day.xlf');
+t3lib_extMgm::allowTableOnStandardPages('tx_frabintegration_domain_model_day');
 $GLOBALS['TCA']['tx_frabintegration_domain_model_day'] = array(
 	'ctrl' => array(
 		'title'	=> 'LLL:EXT:frab_integration/Resources/Private/Language/locallang_db.xlf:tx_frabintegration_domain_model_day',
@@ -126,13 +126,13 @@ $GLOBALS['TCA']['tx_frabintegration_domain_model_day'] = array(
 			'endtime' => 'endtime',
 		),
 		'searchFields' => 'tx_frabintegration_index,date,day_start,day_end,rooms,',
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Day.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_day.gif'
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Day.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_day.gif'
 	),
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_frabintegration_domain_model_event', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_event.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_frabintegration_domain_model_event');
+t3lib_extMgm::addLLrefForTCAdescr('tx_frabintegration_domain_model_event', 'EXT:frab_integration/Resources/Private/Language/locallang_csh_tx_frabintegration_domain_model_event.xlf');
+t3lib_extMgm::allowTableOnStandardPages('tx_frabintegration_domain_model_event');
 $GLOBALS['TCA']['tx_frabintegration_domain_model_event'] = array(
 	'ctrl' => array(
 		'title'	=> 'LLL:EXT:frab_integration/Resources/Private/Language/locallang_db.xlf:tx_frabintegration_domain_model_event',
@@ -155,7 +155,7 @@ $GLOBALS['TCA']['tx_frabintegration_domain_model_event'] = array(
 			'endtime' => 'endtime',
 		),
 		'searchFields' => 'guid,date,start,duration,room,slug,title,subtitle,track,type,language,abstract,description,links,persons,',
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Event.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_event.gif'
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Event.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_frabintegration_domain_model_event.gif'
 	),
 );
